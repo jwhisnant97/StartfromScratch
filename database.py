84771cf83b2fa89a631a8f852a7d6c63b3ab3958
jwhisnant97 <jwhisnant97@gmail.com>
2022-06-13 09:38:07 -0500

    Adding database

---
+++ database.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import psycopg2
+import os
+
+uri = os.getenv("DATABASE_URL")
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://")
+
+conn = psycopg2.connect(uri,sslmode="require")
+
+engine = create_engine(uri)
+
+SessionLocal = sessionmaker(
+    autocommit=False, autoflush=False, bind=engine
+)
+Base = declarative_base()
--- main.py
+++ main.py
@@ -3,11 +3,21 @@
 from fastapi import FastAPI, Depends, HTTPException, Request
 
 # from fastapi.responses import RedirectResponse
-# from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session
 # from starlette.datastructures import URL
+
+# from . import crud, schemas, models
+from .database import SessionLocal, engine
+# from .config import get_settings
 
 app = FastAPI()
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get("/")
 def hello():
