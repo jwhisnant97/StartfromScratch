647da674943c3a150a4adace9fe0bfa2ee22e8ce
jwhisnant97 <jwhisnant97@gmail.com>
2022-06-13 09:25:51 -0500

    put on heroku

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-#StartfromScratch/main.py
+# StartfromScratch/main.py
 
 from fastapi import FastAPI, Depends, HTTPException, Request
-from fastapi.responses import RedirectResponse
-from sqlalchemy.orm import Session
-from starlette.datastructures import URL
+
+# from fastapi.responses import RedirectResponse
+# from sqlalchemy.orm import Session
+# from starlette.datastructures import URL
 
 app = FastAPI()
 
+
 @app.get("/")
 def hello():
-    return {"message":"Hello World!"}
+    return {"message": "Hello World!"}
