06ef0058a264896c94eceeedff1eef3457b78614
jwhisnant97 <jwhisnant97@gmail.com>
2022-06-13 09:08:22 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#StartfromScratch/main.py
+
+from fastapi import FastAPI, Depends, HTTPException, Request
+from fastapi.responses import RedirectResponse
+from sqlalchemy.orm import Session
+from starlette.datastructures import URL
+
+app = FastAPI()
+
+@app.get("/")
+def hello():
+    return {"message":"Hello World!"}
